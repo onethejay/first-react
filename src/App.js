85d35ac9375a3bc83769c195d2ee9956be67951a
onethejay <jhjang951@gmail.com>
2023-04-12 22:40:16 +0900

    useEffect실습1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import './App.css';
+import React from 'react';
+import Info from './Info';
 
-import React, { Component } from 'react';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
-
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-  state = {
-    color: '#000000',
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <Info />;
+};
 
 export default App;
