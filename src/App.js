57cabb4eae90deb4b9ffbbbcd3f6620badf08e31
onethejay <jhjang951@gmail.com>
2023-04-12 22:15:16 +0900

    컴포넌트 반복 실습

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import './App.css';
 import ScrollBox from './ScrollBox';
 
 import React, { Component } from 'react';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
