67d12a167fc29bb0cad3f6965c12eec235b5285f
onethejay <jhjang951@gmail.com>
2023-04-12 22:04:31 +0900

    ref Dom에 이름달기 실습

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
-import EventPractice from './EventPractice';
+import ScrollBox from './ScrollBox';
 
-function App() {
-  return <EventPractice />;
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
